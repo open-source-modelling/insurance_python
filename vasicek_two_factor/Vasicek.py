413728d6238ee5af1d2f48c29687a6f57645525c
(Mostly) Financial Algorithms <95974474+qnity@users.noreply.github.com>
2022-06-26 15:36:39 -0700

    Removed BS and Vasicek 1F functions
    Black Sholes and Vasicek 1 F were moved to their own repository for clarity of purpose

--- vasicek_two_factor/Vasicek.py
+++ vasicek_two_factor/Vasicek.py
@@ -9,7 +9,6 @@ class BrownianMotion():
 
         self.x0 = float(x0)
 
-    # This method generates a Weiner process (more commonly known as a Brownian Motion)
     def generate_weiner_process(self, T: int = 1, dt: float = 0.001, rho: float = None) -> Any:
         # GENERATE_WEINER_PROCESS calculates the sample paths of a one-dimensional Brownian motion or a two-dimensional Brownian motion with a correlation coefficient of rho.
         # The function's output are two sample paths (realisations) of such a process, recorded on increments specified by dt. 
@@ -71,91 +70,6 @@ class BrownianMotion():
                 W_2[iter] = W_2[iter-1] + Z3 # Generate second BM
 
             return [W_1, W_2]
-
-    # This simulates a temporal series of stock prices using the Black Scholes log normal model and the generated Weiner process
-    def simulate_Black_Scholes(self, S0: int = 100, mu: float = 0.05, sigma: float = 0.3, T: int = 52, dt: float = 0.1, rho: float = None) -> pd.DataFrame:
-        # SIMULATE_BLACK_SHOLES calculates a temporal series of stock prices using the Black Scholes log normal model and the generated Brownian motion
-        # stock_price_simulation = simulate_Black_Scholes(self, S0, mu, sigma, T, dt, rho)
-        #
-        # Arguments:
-        #   self  = reference to the current instance of the class. This class includes the x0 parameter that specifies the starting value of the Brownian motion
-        #   S0    = integer, specifying the initial value of the underlying asset
-        #   mu    = float, specifying the drift rate of the underlying asset 
-        #   sigma = float, standard deviation of the underlying asset's return
-        #   T     = integer, specifying the maximum modeling time. ex. if T = 2 then modelling time will run from 0 to 2
-        #   dt    = float, specifying the length of each subinterval. ex. dt=10, then there will be 10 intervals of length 0.1 between two integers of modeling time 
-        #   rho   = float, specifying the correlation coefficient of the Brownian motion. ex. rho = 0.4 means that two 
-        #
-        # Returns:
-        #   stock_price_simulation = N x 2 pandas DataFrame where index is modeling time and values are a realisation of the uderlying's price
-        #
-        # Example:
-        #   import pandas as pd
-        #   import numpy as np
-        #   ToFinish: simulate_Black_Scholes(100,0.05,0.3, 4,0.5,None)   
-        #   [out] =  pd array    
-        #
-        # For more information see SOURCE
-        
-        N = int(T / dt)
-
-        time, delta_t = np.linspace(0, T, num = N, retstep = True)
-
-        stock_variation = (mu - (sigma**2/2)) * time
-
-        weiner_process = sigma * self.generate_weiner_process(T, dt)
-
-        S = S0*(np.exp(stock_variation + weiner_process))
-
-        dict = {'Time' : time, 'Stock Price' : S}
-
-        stock_price_simulation = pd.DataFrame.from_dict(data = dict)
-        stock_price_simulation.set_index('Time', inplace = True)
-
-        return stock_price_simulation
-
-    # This simulates a temporal series of interest rates using the One Factor Vasicek mean reverting model and the generated Weiner process
-    def simulate_Vasicek_One_Factor(self, r0: float = 0.1, a: float = 1.0, b: float = 0.1, sigma: float = 0.2, T: int = 52, dt = 0.1) -> pd.DataFrame:
-        # SIMULATE_VASICEK_ONE_FACTOR simulates a temporal series of interest rates using the One Factor Vasicek mean reverting model and the generated Weiner process
-        #
-        # interest_rate_simulation = simulate_Vasicek_One_Factor(self, r0, a, b, sigma, T, dt)
-        #
-        # Arguments:
-        #   self  = reference to the current instance of the class. This class includes the x0 parameter that specifies the starting value of the Brownian motion
-        #   r0    = float, starting interest rate of the vasicek process 
-        #   a     = float, speed of reversion" parameter thatcharacterizes the velocity at which such trajectories will regroup around b in time
-        #   b     = float, long term mean level. All future trajectories of  r will evolve around a mean level b in the long run  
-        #   sigma = float, instantaneous volatility measures instant by instant the amplitude of randomness entering the system
-        #   T     = integer, end modelling time. From 0 to T the time series runs. 
-        #   dt    = float, increment of time that the proces runs on. Ex. dt = 0.1 then the time series is 0, 0.1, 0.2,...
-        #
-        # Returns:
-        #   interest_rate_simulation = pandas dataframe contains the simulated interest rate process
-        #
-        # Example:
-        #   import pandas as pd
-        #   import numpy as np
-        #   <ToFinish>simulate_Vasicek_One_Factor(0.1, 1.0, 0.1,0.2,52,0.1)   
-        #   [out] =  pd array    
-        # For more information see SOURCE
-        
-        N = int(T / dt)
-
-        time, delta_t = np.linspace(0, T, num = N, retstep = True)
-
-        weiner_process = self.generate_weiner_process(T, dt)
-
-        r = np.ones(N) * r0
-
-        for t in range(1,N):
-            r[t] = r[t-1] + a * (b - r[t-1]) * dt + sigma * (weiner_process[t] - weiner_process[t-1])
-
-        dict = {'Time' : time, 'Interest Rate' : r}
-
-        interest_rate_simulation = pd.DataFrame.from_dict(data = dict)
-        interest_rate_simulation.set_index('Time', inplace = True)
-
-        return interest_rate_simulation
 
     def simulate_Vasicek_Two_Factor(self, r0: List[float] = [0.1, 0.1], a: List[float] = [1.0, 1.0], b: List[float] = [0.1, 0.1], sigma: List[float] = [0.2, 0.2], rho: float = 0.5, T: int = 52, dt: float = 0.1) -> pd.DataFrame:
         # SIMULATE_VASICEK_TWO_FACTOR calculates a posible sample path of the nominal interest rate by simulating the real rate and inflation. Both are assumed to follow a mean-reverting vasicek process
