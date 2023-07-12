5da05e7059d620218788af40f3d41e48fe8b49f7
Open-Source Modelling <95974474+qnity@users.noreply.github.com>
2023-07-13 01:00:37 +0300

    Add files via upload

---
+++ nelson_siegel_svansson/nelsonsiegelsvensson.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy.optimize import minimize
+
+def NelsonSiegelSvansson(T, beta0, beta1, beta2, beta3, lambda0, lambda1):
+    """
+    NelsonSiegelSvansson calculates the interpolated/extrapolated curve at points in the array "T" using the Nelson-Siegel-Svannson algorithm,
+    parameterized with parameters beta0, beta1, beta2, beta3, lambda0, lambda1. It returns a numpy ndarray of points.
+    
+    Arguments:
+        T: n x 1 ndarray of maturities for which the user wants to calculate the corresponding rate.
+        beta0: 1 x 1 floating number, representing the first factor of the NSS parametrization.
+        beta1: 1 x 1 floating number, representing the second factor of the NSS parametrization.
+        beta2: 1 x 1 floating number, representing the third factor of the NSS parametrization.
+        beta3: 1 x 1 floating number, representing the fourth factor of the NSS parametrization.
+        lambda0: 1 x 1 floating number, representing the first shape parameter lambda of the NSS parametrization.
+        lambda1: 1 x 1 floating number, representing the second shape parameter lambda of the NSS parametrization.
+        
+    Returns:
+        n x 1 ndarray of interpolated/extrapolated points corresponding to maturities inside T. Where n is the length of the vector T.
+        
+    Implemented by Gregor Fabjan from Qnity Consultants on  11/07/2023
+    """
+    alpha1 = (1-np.exp(-T/lambda0)) / (T/lambda0)
+    alpha2 = alpha1 - np.exp(-T/lambda0)
+    alpha3 = (1-np.exp(-T/lambda1)) / (T/lambda1) - np.exp(-T/lambda1)
+
+    return beta0 + beta1*alpha1 + beta2*alpha2 + beta3*alpha3
+
+def NSSGoodFit(params, TimeVec, YieldVec):
+    """
+    NSSGoodFit calculates the residuals between the yield predicted by the NSS algorithm with the specified parameterization and the market observed ones.
+    
+    Arguments:
+        params: 6 x 1 tuple containing the 6 parameters of the NSS algorithm. The sequence of parameters needs to be (beta0, ..., beta4, lambda0, lambda1).
+        TimeVec: n x 1 ndarray of maturities for which the yields in YieldVec were observed.
+        YieldVec: n x 1 ndarray of observed yields.
+        
+    Returns:
+        1 x 1 float number Euclidean distance between the calculated points and observed data.
+        
+    Implemented by Gregor Fabjan from Qnity Consultants on  11/07/2023
+    """
+
+    return np.sum((NelsonSiegelSvansson(TimeVec, params[0], params[1], params[2], params[3], params[4], params[5])-YieldVec)**2)
+
+def NSSMinimize(beta0, beta1, beta2, beta3, lambda0, lambda1, TimeVec, YieldVec):
+    """
+    NSSMinimize uses the built-in minimize function from the Python's scipy package. The function sets up the parameters and the function NSSGoodFit as to make it
+    compatible with the way minimize requires its arguments. If the optimization does not converge, the output is an empty array.
+    
+    Arguments:
+        beta0: 1 x 1 floating number, representing the first factor of the NSS parametrization.
+        beta1: 1 x 1 floating number, representing the second factor of the NSS parametrization.
+        beta2: 1 x 1 floating number, representing the third factor of the NSS parametrization.
+        beta3: 1 x 1 floating number, representing the fourth factor of the NSS parametrization.
+        lambda0: 1 x 1 floating number, representing the first shape parameter lambda of the NSS parametrization.
+        lambda1: 1 x 1 floating number, representing the second shape parameter lambda of the NSS parametrization.
+        TimeVec: n x 1 ndarray of maturities for which the yields in YieldVec were observed.
+        YieldVec: n x 1 ndarray of observed yields.
+        
+    Returns:
+        6 x 1 array of parameters and factors that best fit the observed yields (or an empty array if the optimization was not successful).
+        
+    Source:
+    - https://docs.scipy.org/doc/scipy/reference/optimize.minimize-neldermead.html
+    - https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method
+    
+    Implemented by Gregor Fabjan from Qnity Consultants on  11/07/2023
+    """
+
+    opt_sol = minimize(NSSGoodFit, x0=np.array([beta0, beta1, beta2, beta3, lambda0, lambda1]), args = (TimeVec, YieldVec), method="Nelder-Mead")
+    if (opt_sol.success):
+        return opt_sol.x
+    else:
+        return []
---
+++ smith_wilson/SWExtrapolate.py
@@ -0,0 +1,29 @@
+def SWExtrapolate(M_Target, M_Obs, b, ufr, alpha):
+   """
+    Interpolate or extrapolate rates for targeted maturities using the Smith-Wilson algorithm.
+
+    Calculates the rates for maturities specified in `M_Target` using the calibration vector `b` obtained
+    from observed bond maturities in `M_Obs`.
+
+    Arguments:
+        M_Target: k x 1 ndarray representing each targeted bond maturity of interest. Example: M_Target = np.array([[1], [2], [3], [5]])
+        M_Obs: n x 1 ndarray representing the observed bond maturities used for calibrating the calibration vector `b`. Example: M_Obs = np.array([[1], [3]])
+        b: n x 1 ndarray representing the calibration vector calculated on observed bonds.
+        ufr: Floating number representing the ultimate forward rate. Example: ufr = 0.042
+        alpha: Floating number representing the convergence speed parameter alpha. Example: alpha = 0.05
+
+    Returns:
+        k x 1 ndarray representing the targeted rates for zero-coupon bonds. Each rate belongs to a targeted
+        zero-coupon bond with a maturity from `M_Target`. Example: r = np.array([0.0024, 0.0029, 0.0034, 0.0039])
+
+    For more information, refer to the documentation at:
+    https://www.eiopa.europa.eu/sites/default/files/risk_free_interest_rate/12092019-technical_documentation.pdf
+    """
+   import numpy as np 
+   from SWHeart import SWHeart as SWHeart
+   C = np.identity(M_Obs.size)
+   d = np.exp(-np.log(1+ufr) * M_Obs)   # Calculate vector d described in paragraph 138
+   Q = np.diag(d) @ C                   # Matrix Q described in paragraph 139
+   H = SWHeart(M_Target, M_Obs, alpha)  # Heart of the Wilson function from paragraph 132
+   p = np.exp(-np.log(1+ufr)* M_Target) + np.diag(np.exp(-np.log(1+ufr) * M_Target)) @ H @ Q @ b # Discount pricing function for targeted maturities from paragraph 147
+   return p ** (-1/ M_Target) -1 # Convert obtained prices to rates and return prices
