5438dabbda6820b676c40e8f40104cf29280bd28
Open-Source Modelling <95974474+open-source-modelling@users.noreply.github.com>
2023-07-17 01:35:25 +0300

    Update SWHeart.py

--- bisection_alpha/SWHeart.py
+++ bisection_alpha/SWHeart.py
@@ -6,12 +6,12 @@ def SWHeart(u, v, alpha):
         The formula is taken from the EIOPA technical specifications paragraph 132.
 
         Arguments:
-        - u: n_1 x 1 vector of maturities. Example: u = [1, 3]
-        - v: n_2 x 1 vector of maturities. Example: v = [1, 2, 3, 5]
-        - alpha: 1 x 1 floating number representing the convergence speed parameter alpha. Example: alpha = 0.05
+            u: n_1 x 1 vector of maturities. Example: u = [1, 3]
+            v: n_2 x 1 vector of maturities. Example: v = [1, 2, 3, 5]
+            alpha: 1 x 1 floating number representing the convergence speed parameter alpha. Example: alpha = 0.05
 
         Returns:
-        - n_1 x n_2 matrix representing the Heart of the Wilson function for selected maturities and parameter alpha.
+            n_1 x n_2 matrix representing the Heart of the Wilson function for selected maturities and parameter alpha.
         H is calculated as described in paragraph 132 of the EIOPA documentation.
 
         For more information, see:
