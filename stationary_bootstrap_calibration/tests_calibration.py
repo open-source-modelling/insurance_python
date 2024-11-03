717e9e9a471e97ffb5e7e658a13b48cfeb06f6d0
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2024-11-03 19:03:34 +0300

    Unit tests for lam and mlag fyunctions

--- stationary_bootstrap_calibration/tests_calibration.py
+++ stationary_bootstrap_calibration/tests_calibration.py
@@ -10,66 +10,83 @@ def test_output_numpy_array():
     out = lam(x)
     assert isinstance (out, np.ndarray), "Output is not a numpy ndarray."
 
-# Test on interval <0
-def test_result_bellow_zero():
+# Test on interval -1 
+def test_result_bellow_minue_one():
     x = np.array([-1.2])
     out = lam(x)
-    assert out == np.array([0.])
-
-
-# Test lambda on interval 0-0.5; should be 1 
-def test_result_low():
-    x = np.array([0.5])
-    out = lam(x)
-    assert out == np.array([1.])
-
-# Test on interval 0.5 - 1; should be 2(1-|0.7|) = 2*0.3 = 0.6
-def test_lambda_on_interval_mid(): 
-    x = np.array([0.7])
-    out = lam(x)
-    assert out[0] == pytest.approx(0.6,0.00001)
+    assert out[0] == pytest.approx(0., 0.00001), "Output in the inteval bellow -1 is outside expectations."
 
 # Test on interval -0.5 - -1; should be 2(1-|-0.7|)
 def test_lambda_on_interval_mid_negative(): 
     x = np.array([-0.7])
     out = lam(x)
-    assert out == pytest.approx(0.6,0.00001)
+    assert out == pytest.approx(0.6, 0.00001) , "Output in the inteval bellow -0.50 and -1 is outside expectations." 
 
-# Test on interval >1
-x = np.array([1.2])
-out = lam(x)
-print(out)
+# Test on interval -0.5 - 0 
+def test_lambda_on_interval_low_negative():
+    x = np.array([-0.4])
+    out = lam(x)
+    assert out[0] == pytest.approx(1., 0.00001), "Output in the inteval between -0.5 and 0 is outside expectations."
 
+# Test lambda on interval 0-0.5; should be 1 
+def test_lambda_on_interval_low_positive():
+    x = np.array([0.3])
+    out = lam(x)
+    assert out == np.array([1.]), "Output in the inteval bellow 0 and 0.5 is outside expectations." 
+
+# Test on interval 0.5 - 1; should be 2(1-|0.7|) = 2*0.3 = 0.6
+def test_lambda_on_interval_mid_positive():
+    x = np.array([0.7])
+    out = lam(x)
+    assert out[0] == pytest.approx(0.6,0.00001), "Output in the inteval bellow 0.5 and 1 is outside expectations." 
+
+# Test on interval  bigger than 1; should be 0
+def test_lambda_on_interval_high_positive():
+    x = np.array([2.3])
+    out = lam(x)
+    assert out[0] == pytest.approx(0.,0.00001), "Output in the inteval bigger than 1 is outside expectations." 
 
 # Test on multiple outputs
-x = np.array([-0.2, 0.1, 0.6, 0.8, 1.1])
-out = lam(x)
-print(out)
+def test_lambda_multiple_outputs():
+    x = np.array([-0.2, 0.1, 0.6, 0.8, 1.1])
+    out = lam(x)
+    assert out.size == 5, "Output is of different size than input."
+
+#Test lam output type
+def test_mlag_numpy_array():
+    x = np.array([1,2,3,4])
+    n = 2
+    assert isinstance(mlag(x,n), np.ndarray), "Output is not a numpy ndarray."
+
+# Test mlag  size
+def test_mlag_typical_input():
+    x = np.array([1,2,3,4])
+    n = 2
+    assert mlag(x,n).shape == (4,2)
 
 # Test mlag normal
-x = np.array([1,2,3,4])
-n = 2
-print(mlag(x,n))
+def test_mlag_typical_input():
+    x = np.array([1,2,3,4])
+    n = 2
+    out_hardcoded = np.array([[0,0], [1,0], [2,1], [3,2]])
+    out = mlag(x,n)
+    assert np.array_equal(out, out_hardcoded), "Typical output 1,2,3,4 is not as originaly expected."
      
-# Test mlag normal size
-x = np.array([1,2,3,4])
-n = 2
-print(mlag(x,n).shape)
-
 # Test mlag single input
-x = np.array([1])
-n = 2
-print(mlag(x,n))
+def test_mlag_single_input():
+    x = np.array([1])
+    n = 2
+    out = mlag(x,n)
+    out_hardcoded = np.array([[0,0]])
+    assert np.array_equal(out, out_hardcoded)
 
-# Test mlag single input
-x = np.array([1,2,3])
-n = 1
-print(mlag(x,n))
-
-# Test mlag single input
-x = np.array([1,2,3])
-n = 1
-print(mlag(x,n))
+# Test mlag single lag
+def test_mlag_single_lag():
+    x = np.array([1,2,3])
+    n = 1
+    out = mlag(x,n)
+    out_hardcoded = np.array([[0.],[1.],[2.]])
+    assert np.array_equal(out, out_hardcoded)
 
 # Test OptimalLength
 data = np.array([0.4,0.2,0.1,0.4,0.3,0.1,0.3,0.4,0.2,0.5,0.1,0.2])
